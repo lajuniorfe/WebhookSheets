5d8e2b99300a0f32f3b55c2359b8c7f3ca07c653
lajuniorfe <33765211+lajuniorfe@users.noreply.github.com>
2025-05-20 16:16:02 -0300

    Ajusta alerta para verificar recebimento

--- WebhookSheets.Api/Controller/PlanilhaFinanceiro/PlanilhaFinanceiroController.cs
+++ WebhookSheets.Api/Controller/PlanilhaFinanceiro/PlanilhaFinanceiroController.cs
@@ -14,13 +14,14 @@ namespace WebhookSheets.Controller.PlanilhaFinanceiro
         [HttpPost]
         public ActionResult ReceberAlteracaoPlanilhaFinanceiro([FromBody] GoogleSheetFinanceiroRequest request)
         {
-            Console.WriteLine("testando", request);
-
             Console.WriteLine($"Recebido evento: {request.Evento}");
             Console.WriteLine($"Aba: {request.Sheet}, Linha: {request.Row}");
             Console.WriteLine($"Data/Hora: {request.Time}");
-            Console.WriteLine($"Valores: {request.Values}");
+            foreach (var i in request.Values)
+            {
+                Console.WriteLine($"Valores: {i}");
 
+            }
 
             return Ok();
         }
