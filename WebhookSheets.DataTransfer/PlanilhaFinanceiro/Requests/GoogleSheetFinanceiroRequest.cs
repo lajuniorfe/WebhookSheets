775286135c37e39472c3f2a1e1cbfff936f4f3df
lajuniorfe <33765211+lajuniorfe@users.noreply.github.com>
2025-05-20 14:30:12 -0300

    Adicionar arquivos de projeto.

---
+++ WebhookSheets.DataTransfer/PlanilhaFinanceiro/Requests/GoogleSheetFinanceiroRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebhookSheets.DataTransfer.PlanilhaFinanceiro.Requests
+{
+    public class GoogleSheetFinanceiroRequest
+    {
+        public string Evento { get; set; } // "FORM_SUBMIT"
+        public string Sheet { get; set; }  // Nome da aba
+        public int Row { get; set; }       // Número da linha inserida
+        public List<string> Values { get; set; } // Dados da linha
+        public Dictionary<string, List<string>> Named { get; set; } // Pergunta → [resposta]
+        public DateTime Time { get; set; }  // Timestamp ISO 8601
+    }
+}
---
+++ WebhookSheets/Controller/PlanilhaFinanceiro/PlanilhaFinanceiroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebhookSheets.DataTransfer.PlanilhaFinanceiro.Requests;
+
+namespace WebhookSheets.Controller.PlanilhaFinanceiro
+{
+    [Route("api/financeiro")]
+    [ApiController]
+    public class PlanilhaFinanceiroController : ControllerBase
+    {
+        public PlanilhaFinanceiroController()
+        {
+        }
+
+        [HttpPost]
+        public ActionResult ReceberAlteracaoPlanilhaFinanceiro([FromBody] GoogleSheetFinanceiroRequest request)
+        {
+            Console.WriteLine("testando", request);
+
+            Console.WriteLine($"Recebido evento: {request.Evento}");
+            Console.WriteLine($"Aba: {request.Sheet}, Linha: {request.Row}");
+            Console.WriteLine($"Data/Hora: {request.Time}");
+            Console.WriteLine($"Valores: {request.Values}");
+
+
+            return Ok();
+        }
+    }
+}
